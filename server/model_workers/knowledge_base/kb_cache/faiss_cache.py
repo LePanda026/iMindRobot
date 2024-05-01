1f0faeb053306c6abdc8d2efa1141487fe7ce697
LePanda026 <106538913+LePanda026@users.noreply.github.com>
2024-05-02 00:03:34 +0800

    Update faiss_cache.py

--- server/model_workers/knowledge_base/kb_cache/faiss_cache.py
+++ server/model_workers/knowledge_base/kb_cache/faiss_cache.py
@@ -76,7 +76,7 @@ class KBFaissPool(_FaissPool):
 
                 if os.path.isfile(os.path.join(vs_path, "index.faiss")):
                     embeddings = self.load_kb_embeddings(kb_name=kb_name, embed_device=embed_device)
-                    vector_store = FAISS.load_local(vs_path, embeddings, normalize_L2=True)
+                    vector_store = FAISS.load_local(vs_path, embeddings, normalize_L2=True , allow_dangerous_deserialization=True)
                 elif create:
                     # create an empty vector store
                     if not os.path.exists(vs_path):
